40141e35a67d9bed1b5f99340ec4411ef56550ff
bladewind21 <bladewind21@gmail.com>
2022-09-22 15:29:47 -0500

    initializing spring boot graphql example

---
+++ src/main/java/graphql/demo/graphqlspringbootstarterexample/GraphqlSpringbootStarterExampleApplication.java
@@ -0,0 +1,13 @@
+package graphql.demo.graphqlspringbootstarterexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlSpringbootStarterExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlSpringbootStarterExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/graphql/demo/graphqlspringbootstarterexample/GraphqlSpringbootStarterExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package graphql.demo.graphqlspringbootstarterexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlSpringbootStarterExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
